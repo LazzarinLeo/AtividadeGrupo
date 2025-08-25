af639954cf1769930c9d83d4cb1f90a0e916f95e
Leonardo Lazzarin <lnrdlazzarin@gmail.com>
2025-08-25 11:05:27 -0300

    JS Funcionando

--- script.js
+++ script.js
@@ -1,19 +1,55 @@
-const botoes = document.querySelectorAll("button");
-let acertos = 0
-botoes.forEach(botao => {
+let acertos = 0;
+
+esconderDiv("pergunta2");
+esconderDiv("pergunta3");
+esconderDiv("pergunta4");
+esconderDiv("pergunta5");
+
+
+document.querySelectorAll("#pergunta1 button").forEach(botao => {
   botao.addEventListener('click', () => {
-    if (botao.textContent === "Júpiter") {
-      esconderDiv("pergunta1");
-      acertos++
-      mostrarDiv("pergunta2");
-    }
-    else{
-        esconderDiv("pergunta1");
-        console.log(acertos);
-        mostrarDiv("pergunta2");
-    }
+    esconderDiv("pergunta1");
+    if (botao.textContent.trim() === "Júpiter") acertos++;
+    mostrarDiv("pergunta2");
   });
 });
+
+
+document.querySelectorAll("#pergunta2 button").forEach(botao => {
+  botao.addEventListener('click', () => {
+    esconderDiv("pergunta2");
+    if (botao.textContent.trim() === "Peru") acertos++;
+    mostrarDiv("pergunta3");
+  });
+});
+
+document.querySelectorAll("#pergunta3 button").forEach(botao => {
+  botao.addEventListener('click', () => {
+    esconderDiv("pergunta3");
+    if (botao.textContent.trim() === "Thomas Edison") acertos++;
+    mostrarDiv("pergunta4");
+  });
+});
+
+document.querySelectorAll("#pergunta4 button").forEach(botao => {
+  botao.addEventListener('click', () => {
+    esconderDiv("pergunta4");
+    if (botao.textContent.trim() === "100ºC") acertos++;
+    mostrarDiv("pergunta5");
+  });
+});
+
+document.querySelectorAll("#pergunta5 button").forEach(botao => {
+  botao.addEventListener('click', () => {
+    esconderDiv("pergunta5"); 
+     
+    if (botao.textContent.trim() === "206") acertos++;
+  alert("Você acertou" + acertos+ "/5" )
+});
+
+});
+
+
 
 function esconderDiv(pergunta) {
   document.getElementById(pergunta).style.display = "none";
@@ -22,3 +58,4 @@ function esconderDiv(pergunta) {
 function mostrarDiv(pergunta) {
   document.getElementById(pergunta).style.display = "block";
 }
+
